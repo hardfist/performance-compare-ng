6b48f0c5833b8a24861037331de43fff3b3146b4
yangjian.fe <yangjian.fe@bytedance.com>
2024-10-23 21:27:05 +1100

    chore: add large file

--- apps/10000-big/react.js
+++ apps/10000-big/react.js
@@ -1,3 +1,5 @@
+;
+
 /**
  * @license React
  * react.development.js
--- apps/10000-big/scripts.js
+++ apps/10000-big/scripts.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const srcDir = path.join(__dirname, 'src');
-const functionCode = fs.readFileSync('./react.js','utf-8')
+  const functionCode = fs.readFileSync('./react.js', 'utf-8');
 function addFunctionToFile(filePath) {
+  
 
     try {
         const data = fs.readFileSync(filePath, 'utf8');
@@ -15,14 +16,20 @@ function addFunctionToFile(filePath) {
     }
 }
 
-try {
-    const files = fs.readdirSync(srcDir);
-    files.forEach(file => {
-        const filePath = path.join(srcDir, file);
-        if (fs.lstatSync(filePath).isFile()) {
-            addFunctionToFile(filePath);
-        }
-    });
-} catch (err) {
-    console.error('Error reading src directory:', err);
+function processDirectory(directory) {
+    try {
+        const items = fs.readdirSync(directory);
+        items.forEach(item => {
+            const itemPath = path.join(directory, item);
+            if (fs.lstatSync(itemPath).isDirectory()) {
+                processDirectory(itemPath);
+            } else if (fs.lstatSync(itemPath).isFile()) {
+                addFunctionToFile(itemPath);
+            }
+        });
+    } catch (err) {
+        console.error(`Error reading directory ${directory}:`, err);
+    }
 }
+
+processDirectory(srcDir);
